64ecd615c797f0654b124055bc7f606878afa8ac
t-chito <published.address.of.k.t@gmail.com>
2024-06-21 15:50:02 +0900

    MFA で range 指定と、unknown の単語を replace した版での処理を試した。
    - TextGrid で range 指定
    - Backstabbing -> Back stabbing

--- scripts/mfa.sh
+++ scripts/mfa.sh
@@ -11,10 +11,10 @@ mfa model download dictionary english_us_arpa
 # 動作に必要なファイルの確認
 # https://montreal-forced-aligner.readthedocs.io/en/latest/user_guide/data_validation.html
 echo "----- validate files -----"
-mfa validate ${CORPUS_DIRECTORY} english_us_arpa english_us_arpa --single_speaker --beam 100 --retry_beam 400 --verbose --num_jobs 1 --use_postgres # --temporary_directory  ${CORPUS_DIRECTORY}/temp --no_final_clean
+mfa validate ${CORPUS_DIRECTORY} english_us_arpa english_us_arpa --single_speaker --beam 100 --retry_beam 400 --verbose --num_jobs 1 --use_postgres --no_final_clean # --temporary_directory  ${CORPUS_DIRECTORY}/temp
 
 # alignment の実行
 # https://montreal-forced-aligner.readthedocs.io/en/latest/user_guide/workflows/alignment.html#mfa-align
 # mfa align [OPTIONS] CORPUS_DIRECTORY DICTIONARY_PATH ACOUSTIC_MODEL_PATH OUTPUT_DIRECTORY
 echo "----- align -----"
-mfa align ${CORPUS_DIRECTORY} english_us_arpa english_us_arpa ${CORPUS_DIRECTORY} --single_speaker --beam 100 --retry_beam 400 --verbose --num_jobs 1 --use_postgres
+mfa align ${CORPUS_DIRECTORY} english_us_arpa english_us_arpa ${CORPUS_DIRECTORY}/results --single_speaker --beam 100 --retry_beam 400 --verbose --num_jobs 1 --use_postgres
