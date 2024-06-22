40d07871c47e3b193b7bef4651e6b345e180701a
t-chito <published.address.of.k.t@gmail.com>
2024-06-22 22:46:41 +0900

    stable-ts のアライメント処理を実装した

---
+++ vocal_score_converter/my_stable-ts/main.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import stable_whisper
+
+# settings
+# Path を受け付けていないようなので、絶対パスの str で定義する
+audio_file = str(Path("songs/Recreant/Recreant_cut.wav").resolve())
+results_dir = Path("songs/Recreant/4stable-ts").resolve()
+language = "en"
+
+start = 18.0
+end = 226.0
+
+with open(Path("songs/Recreant/Recreant_onel.txt"), "r") as f:
+    lyrics = f.read()
+
+# Alignment https://github.com/jianfch/stable-ts?tab=readme-ov-file#alignment
+model = stable_whisper.load_model("large-v2")
+
+# アラインメントを行う
+result = model.align(audio_file, lyrics, language=language)
+result.to_srt_vtt(str(results_dir / "align.srt"))
+
+# 再度アラインメントを行う
+new_result = model.align(audio_file, result, language=language)
+new_result.to_srt_vtt(str(results_dir / "align2.srt"))
+
+# result を new_result で adjust する
+result.adjust_by_result(new_result)
+result.to_srt_vtt(str(results_dir / "adjusted_align.srt"))
+
+# refine する TODO: これは forced alignment なのか？ -> 多分違うのでコメントアウト
+# model.refine("audio.mp3", result)
